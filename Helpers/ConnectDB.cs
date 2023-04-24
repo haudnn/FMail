3aa0b06eb19599dde3dcb2adadbe21931860ba5c
S3C0ND19 <90059594+S3C0ND19@users.noreply.github.com>
2023-04-24 09:13:12 +0700

    update:  connectdb

--- Helpers/ConnectDB.cs
+++ Helpers/ConnectDB.cs
@@ -7,8 +7,7 @@ public class ConnectDB
     private static readonly IMongoClient _client;
     static ConnectDB()
     {
-        
-        string DB_NAME = "mongodb+srv://dnhau191:L0ojM4eciv1rD9Km@mailbox.gzf7jht.mongodb.net/test";
+        string DB_NAME = "mongodb+srv://dnhau191:L0ojM4eciv1rD9Km@mailbox.gzf7jht.mongodb.net/?retryWrites=true&w=majority";
         var settings = MongoClientSettings.FromConnectionString(DB_NAME);
         _client = new MongoClient(settings);
     }
